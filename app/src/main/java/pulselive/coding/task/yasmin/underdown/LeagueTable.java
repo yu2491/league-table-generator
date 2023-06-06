9fa79a2d806a2c375318ee588cbe22cd1bb9364c
Yasmin Underdown <yunderdo@ford.com>
2023-06-06 01:19:48 +0100

    Updating saved entry rather than creating a new entry for same team.

--- app/src/main/java/pulselive/coding/task/yasmin/underdown/LeagueTable.java
+++ app/src/main/java/pulselive/coding/task/yasmin/underdown/LeagueTable.java
@@ -6,44 +6,76 @@ import java.util.List;
 public class LeagueTable {
     private final List<Match> matches;
 
+    private final List<LeagueTableEntry> tableEntries = new ArrayList<>();
+
     public LeagueTable(final List<Match> matches) {
         this.matches = new ArrayList<>(matches);
     }
 
+    public void addMatch(Match match) {
+        matches.add(match);
+    }
+
     public List<LeagueTableEntry> getTableEntries() {
-        List<LeagueTableEntry> tableEntries = new ArrayList<>();
         for (Match match : matches) {
-            addTableEntry(tableEntries, match.getHomeTeam(), match.getHomeScore(), match.getAwayScore());
-            addTableEntry(tableEntries, match.getAwayTeam(), match.getAwayScore(), match.getHomeScore());
+            addOrUpdateLeagueTableEntry(match.getHomeTeam(), match.getHomeScore(), match.getAwayScore());
+            addOrUpdateLeagueTableEntry(match.getAwayTeam(), match.getAwayScore(), match.getHomeScore());
         }
         return tableEntries;
     }
 
-    private void addTableEntry(List<LeagueTableEntry> tableEntries, String teamName, int goalsFor, int goalsAgainst) {
-        int goalDifference = goalsFor - goalsAgainst;
-        LeagueTableEntry tableEntry = new LeagueTableEntry().builder()
-                .teamName(teamName)
-                .played(1)
-                .goalsFor(goalsFor)
-                .goalsAgainst(goalsAgainst)
-                .goalDifference(goalDifference)
-                .build();
-        if (goalDifference == 0) {
-            tableEntry.setWon(0);
-            tableEntry.setDrawn(1);
-            tableEntry.setLost(0);
-            tableEntry.setPoints(1);
-        } else if (goalDifference > 0) {
-            tableEntry.setWon(1);
-            tableEntry.setDrawn(0);
-            tableEntry.setLost(0);
-            tableEntry.setPoints(3);
-        } else {
-            tableEntry.setWon(0);
-            tableEntry.setDrawn(0);
-            tableEntry.setLost(1);
-            tableEntry.setPoints(0);
+    private void addOrUpdateLeagueTableEntry(String teamName, int goalsFor, int goalsAgainst) {
+        LeagueTableEntry tableEntry = getTeamFromTable(teamName);
+        if(tableEntry == null) {
+            tableEntry = newTeamLeagueTableEntry(teamName);
+            tableEntries.add(tableEntry);
         }
-        tableEntries.add(tableEntry);
+        tableEntry.setPlayed(tableEntry.getPlayed() + 1);
+        tableEntry.setGoalsFor(tableEntry.getGoalsFor() + goalsFor);
+        tableEntry.setGoalsAgainst(tableEntry.getGoalsAgainst() + goalsAgainst);
+        int goalDifference = goalsFor - goalsAgainst;
+        tableEntry.setGoalDifference(tableEntry.getGoalDifference() + goalDifference);
+        if (goalDifference == 0) {
+            updateEntryForDraw(tableEntry);
+        } else if (goalDifference > 0) {
+            updateEntryForWin(tableEntry);
+        } else {
+            updateEntryForLoss(tableEntry);
+        }
+    }
+
+    private LeagueTableEntry newTeamLeagueTableEntry(String teamName) {
+        return new LeagueTableEntry().builder()
+                .teamName(teamName)
+                .played(0)
+                .won(0)
+                .drawn(0)
+                .lost(0)
+                .goalsFor(0)
+                .goalsAgainst(0)
+                .goalDifference(0)
+                .points(0)
+                .build();
+    }
+
+    private LeagueTableEntry getTeamFromTable(String teamName) {
+        for (LeagueTableEntry entry : tableEntries) {
+            if (entry.getTeamName().equals(teamName)) {
+                return entry;
+            }
+        }
+        return null;
+    }
+
+    private static void updateEntryForWin(LeagueTableEntry tableEntry) {
+        tableEntry.setWon(tableEntry.getWon() + 1);
+        tableEntry.setPoints(tableEntry.getPoints() + 3);
+    }
+    private static void updateEntryForLoss(LeagueTableEntry tableEntry) {
+        tableEntry.setLost(tableEntry.getLost() + 1);
+    }
+    private static void updateEntryForDraw(LeagueTableEntry tableEntry) {
+        tableEntry.setDrawn(tableEntry.getDrawn() + 1);
+        tableEntry.setPoints(tableEntry.getPoints() + 1);
     }
 }
--- app/src/test/java/pulselive/coding/task/yasmin/underdown/LeagueTableTest.java
+++ app/src/test/java/pulselive/coding/task/yasmin/underdown/LeagueTableTest.java
@@ -12,21 +12,24 @@ public class LeagueTableTest {
 
     List<LeagueTableEntry> tableEntries;
 
+    LeagueTable leagueTable;
+
     @Before
     public void setUp() {
         List<Match> matches = new ArrayList<>();
         matches.add(new Match("Team A", "Team B", 2, 1));
-        LeagueTable leagueTable = new LeagueTable(matches);
-        tableEntries = leagueTable.getTableEntries();
+        leagueTable = new LeagueTable(matches);
     }
 
     @Test
     public void addingMatchToLeagueTableShouldAddTwoLeagueTableEntries() {
+        tableEntries = leagueTable.getTableEntries();
         assertEquals(2, tableEntries.size());
     }
 
     @Test
     public void teamAStats() {
+        tableEntries = leagueTable.getTableEntries();
         assertEquals("Team A", tableEntries.get(0).getTeamName());
         assertEquals(1, tableEntries.get(0).getPlayed());
         assertEquals(1, tableEntries.get(0).getWon());
@@ -40,6 +43,7 @@ public class LeagueTableTest {
 
     @Test
     public void teamBStats() {
+        tableEntries = leagueTable.getTableEntries();
         assertEquals("Team B", tableEntries.get(1).getTeamName());
         assertEquals(1, tableEntries.get(1).getPlayed());
         assertEquals(0, tableEntries.get(1).getWon());
@@ -50,6 +54,17 @@ public class LeagueTableTest {
         assertEquals(-1, tableEntries.get(1).getGoalDifference());
         assertEquals(0, tableEntries.get(1).getPoints());
     }
+
+    @Test
+    public void whenAddingAMatchShouldAddToExistingLeagueTableEntryIfTeamAlreadyExists() {
+        Match match = new Match("Team B", "Team A", 2, 2);
+        leagueTable.addMatch(match);
+        tableEntries = leagueTable.getTableEntries();
+        assertEquals(2, tableEntries.size());
+        assertEquals(2, tableEntries.get(0).getPlayed());
+        assertEquals(2, tableEntries.get(1).getPlayed());
+    }
+
 
 
 }
